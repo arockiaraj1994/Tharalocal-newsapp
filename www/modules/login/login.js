264324ea222c12eb975d68c47b86b5eafe2a97b5
sivanarayanan-dts <ganeshsiva098@gmail.com>
2016-10-03 19:21:21 +0530

    Added the news view page and signup page

--- www/modules/login/login.js
+++ www/modules/login/login.js
@@ -1,7 +1,19 @@
-TharaLocal.controller("LoginController", ['$scope','$location', function($scope, $location) {
+TharaLocal.controller("LoginController", ['$scope','$location','$http', function($scope, $location, $http) {
 
     $scope.login = function() {
         $location.path('/home')
+    }
+    
+    $scope.signUp = function() {
+        $http({
+            url: '',
+            method:'POST',
+            
+        }).then(function successCallback(response) {
+            
+        }, function errorCallback(response) {
+            
+        });
     }
     
 }])
